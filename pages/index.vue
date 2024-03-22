defd793140a1ddc739348dcad8654d78906c393d
Danil Falaleev <danilkafalaleev@gmail.com>
2024-03-22 07:46:34 +0300

    landing

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,22 @@
 <template>
-	<UContainer class="text-4xl text-center grid place-content-center h-screen">
-		<h1>MAIN PAGE</h1>
-		<NuxtLink
-			to="login"
-			class="text-center text-lg bg-transparent my-4 p-1 hover:bg-mountain-500 text-black-700 font-bold hover:font-medium hover:text-white border-4 border-mountain-500 hover:border-transparent rounded-lg"
-			>Войти
-		</NuxtLink>
-	</UContainer>
+    <UContainer class=" text-xl max-w-xl text-center my-auto place-content-center h-screen">
+        <h1 class="text-4xl font-bold">Добро пожаловать в наш магазин конструктора интернет магазина в telegram!</h1>
+        <p class='py-4'>Создайте своего персонального телеграм-бота всего в несколько кликов.</p>
+        <NuxtLink to="login"
+            class="rounded-lg bg-mountain-600 text-sm font-semibold text-white shadow-sm hover:bg-mountain-500
+    focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-mountain-600 px-5 py-2">
+            Войти
+        </NuxtLink>
+    </UContainer>
+    <div class="absolute bg-repeat h-screen her"></div>
 </template>
 
 <script setup lang="ts">
 definePageMeta({
-	layout: "default",
-	title: "welcome",
+    middleware: "already-logged-in",
 });
+
 useHead({ title: "Welcome" });
 </script>
+
+<style></style>
