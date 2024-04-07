b2f2595c805fded2d11a8fecf2122892e9368294
Falaleev Danil <danilkafalaleev@gmail.com>
2024-04-07 16:50:24 +0300

    добавлены категории, и фильтрация по товарам

--- pages/goods.vue
+++ pages/goods.vue
@@ -4,13 +4,17 @@
             <div class="animate-spin rounded-full h-32 w-32 border-b-2 border-gray-900"></div>
         </div>
         <div v-else class="">
-            <button
-                class="text-center bg-transparent hover:bg-pale-sky-100 my-auto shadow-lg dark:shadow-neutral-700/50 grow text-black-700 font-semibold p-1 border-2 border-pale-sky-900 dark:border-neutral-400 hover:dark:bg-neutral-800 rounded-lg animate__animated hovanimate__swing ml-3">
-                Пицца
-            </button>
+            <div class="flex flex-wrap">
+                <button v-for="category in categories.results" :key="category.id"
+                    @click="selectedCategory = category.id"
+                    class="text-center bg-transparent hover:bg-pale-sky-400 my-auto shadow-lg dark:shadow-neutral-700/50 text-black-700 font-semibold p-1 border-2 border-pale-sky-900 dark:border-neutral-400 hover:dark:bg-neutral-800 rounded-lg animate__animated hovanimate__swing ml-3 mb-3"
+                    :class="{ 'bg-pale-sky-300 dark:bg-pale-sky-600': category.id === selectedCategory }">
+                    {{ category.title }}
+                </button>
+            </div>
             <ul>
-                <div class="mt-6 grid grid-cols-2 gap-x-6 gap-y-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5">
-                    <div v-for="product in products.results" :key="product.id" class="group relative">
+                <div class=" mt-6 grid grid-cols-2 gap-x-6 gap-y-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6">
+                    <div v-for="product in filteredProducts" :key="product.id" class="group relative">
                         <a href="" class="animate__animated animate__fadeIn">
                             <div
                                 class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75">
@@ -43,13 +47,23 @@ definePageMeta({
 });
 useHead({ title: "Goods" });
 
+
+
 interface Product {
     count: number;
     results: {
         id: string;
         title: string;
         price: number;
+        category: string;
         images: string;
+    }[];
+}
+interface Category {
+    count: number;
+    results: {
+        id: string;
+        title: string;
     }[];
 }
 
@@ -58,17 +72,56 @@ const products = ref<Product>({
     results: []
 });
 
-const loading = ref(true);
+const categories = ref<Category>({
+    count: 0,
+    results: []
+});
 
-API.get('products/')
-    .then(function (response) {
-        products.value = response.data;
-        console.log(products);
+const selectedCategory = ref('');
+
+const loading = ref(true);
+const filteredProducts = ref<Product['results']>([]);
+
+Promise.all([
+    API.get('categories/'),
+    API.get('products/')
+])
+    .then(function ([categoriesResponse, productsResponse]: any) {
+        categories.value = categoriesResponse.data;
+        products.value = productsResponse.data;
+
+        if (categories.value.results.length > 0) {
+            selectedCategory.value = categories.value.results[0].id;
+            filterProducts();
+        } else {
+            filteredProducts.value = products.value.results;
+        }
+
         loading.value = false;
     })
-    .catch(function (error) {
+    .catch(function (error: any) {
         console.log(error);
     });
+
+function filterProducts() {
+    if (selectedCategory.value === '') {
+        filteredProducts.value = products.value.results;
+    } else {
+        filteredProducts.value = products.value.results.filter(product =>
+            product.category.toLowerCase() === selectedCategory.value.toLowerCase()
+        );
+    }
+}
+
+watch(selectedCategory, (newValue) => {
+    if (newValue === '') {
+        filteredProducts.value = products.value.results;
+    } else {
+        filteredProducts.value = products.value.results.filter(product =>
+            product.category.toLowerCase() === newValue.toLowerCase()
+        );
+    }
+});
 
 const { logout } = actions();
 </script>
