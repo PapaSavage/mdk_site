436a1b9e21d01f6c931b86869c179763f6098b99
Pavel Suslov <Pavel.suslow2018@yandex.ru>
2024-04-19 02:31:29 +0300

    home

--- pages/home.vue
+++ pages/home.vue
@@ -1,12 +1,18 @@
 <template>
     <div class="grow grid place-content-top h-screen">
         <h1 class="page-title">Создание бота в телеграм</h1>
-        <div class="grow grid place-content-top h-screen">
-            <p class=" paragraph"> 1. Для получения токена бота телеграмм, нужно зайти в <a
-                    href="https://t.me/BotFather" style="color: blue; text-decoration: underline">телеграмм бота</a>,
-                после чего вызвать бота с помощью кнопки "start"</p>
-            <img src="~/assets/img/BotFather.png">
-            <p>ыыы</p>
+        <div class="image-container">
+            <p class="paragraph">1. Для получения токена бота телеграмм, нужно зайти в <a href="https://t.me/BotFather"
+                    style="color: blue; text-decoration: underline">телеграмм бота</a>, после чего вызвать бота с
+                помощью кнопки "start"</p>
+            <img class="animate-slide-in" src="~/assets/img/BotFather.png">
+            <p class="paragraph">2. Нажать кнопку "newbot"</p>
+            <img class="animate-slide-in" src="~/assets/img/NewBot.png">
+            <p class="paragraph">3. После чего нужно придумать имя бота, имя пользователя, которое будет оканчиваться
+                "bot"</p>
+            <img class="animate-slide-in" src="~/assets/img/NameBot.png">
+            <p class="paragraph">4. Поздравляю создан токен бота</p>
+            <img class="animate-slide-in" src="~/assets/img/Token.png">
         </div>
     </div>
 </template>
@@ -23,13 +29,36 @@
     text-align: center;
     font-size: 16px;
     color: #333;
+    margin-top: 40px;
+}
+
+.image-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 }
 
 img {
-    display: block;
-    margin: -100px auto 0px;
-    max-width: 100%;
+    opacity: 0;
+    max-width: 40%;
+    max-height: 100xp;
     height: auto;
+}
+
+.animate-slide-in {
+    animation: slideIn 0.5s ease forwards;
+}
+
+@keyframes slideIn {
+    from {
+        opacity: 0;
+        transform: translateY(-20px);
+    }
+
+    to {
+        opacity: 1;
+        transform: translateY(0);
+    }
 }
 </style>
 
