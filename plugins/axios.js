7c2ffdb9bebd92324030191f26efe1763c30bdd3
Danil Falaleev <danilkafalaleev@gmail.com>
2024-04-19 02:04:19 +0300

    change api key

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const API = axios.create({
-    baseURL: 'http://127.0.0.1:9010/',
+    baseURL: 'https://mdk-api.onrender.com/',
     validateStatus(status) {
         return status >= 200 && status < 500
     }
