2083d0605be4e60b6106cf26db9538fbbb9f20dc
Danil Falaleev <danilkafalaleev@gmail.com>
2024-04-25 22:08:10 +0300

    change style homepage

--- pages/home.vue
+++ pages/home.vue
@@ -1,66 +1,27 @@
 <template>
-    <div class="grow grid place-content-top h-screen">
-        <h1 class="page-title">Создание бота в телеграм</h1>
-        <div class="image-container">
-            <p class="paragraph">1. Для получения токена бота телеграмм, нужно зайти в <a href="https://t.me/BotFather"
-                    style="color: blue; text-decoration: underline">телеграмм бота</a>, после чего вызвать бота с
-                помощью кнопки "start"</p>
-            <img class="animate-slide-in" src="~/assets/img/BotFather.png">
-            <p class="paragraph">2. Нажать кнопку "newbot"</p>
-            <img class="animate-slide-in" src="~/assets/img/NewBot.png">
-            <p class="paragraph">3. После чего нужно придумать имя бота, имя пользователя, которое будет оканчиваться
-                "bot"</p>
-            <img class="animate-slide-in" src="~/assets/img/NameBot.png">
-            <p class="paragraph">4. Поздравляю создан токен бота</p>
-            <img class="animate-slide-in" src="~/assets/img/Token.png">
+    <div class="grow grid h-screen animate__animated animate__fadeIn text-lg overflow-y-auto">
+        <div class="my-auto mx-auto text-center">
+
+            <h1 class="py-10 text-5xl font-bold">Создание бота в телеграм</h1>
+            <div class="text-xl">
+                <p class="py-4">1. Для получения токена бота телеграмм, нужно зайти в <a href="https://t.me/BotFather">
+                        телеграмм
+                        бота</a>, после чего вызвать бота с
+                    помощью кнопки "start"</p>
+                <img class=" mx-auto" src="~/assets/img/BotFather.png">
+                <p class="py-4">2. Нажать кнопку "newbot"</p>
+                <img class="animate-slide-in  mx-auto" src="~/assets/img/NewBot.png">
+                <p class="paragraph">3. После чего нужно придумать имя бота, имя пользователя, которое будет
+                    оканчиваться
+                    "bot"</p>
+                <img class="animate-slide-in  mx-auto" src="~/assets/img/NameBot.png">
+                <p class="py-4">4. Поздравляю создан токен бота</p>
+                <img class="animate-slide-in" src="~/assets/img/Token.png">
+            </div>
         </div>
     </div>
 </template>
 
-<style scoped>
-.page-title {
-    margin-bottom: 20px;
-    text-align: center;
-    font-weight: bold;
-    font-size: 32px;
-}
-
-.paragraph {
-    text-align: center;
-    font-size: 16px;
-    color: #333;
-    margin-top: 40px;
-}
-
-.image-container {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-}
-
-img {
-    opacity: 0;
-    max-width: 40%;
-    max-height: 100xp;
-    height: auto;
-}
-
-.animate-slide-in {
-    animation: slideIn 0.5s ease forwards;
-}
-
-@keyframes slideIn {
-    from {
-        opacity: 0;
-        transform: translateY(-20px);
-    }
-
-    to {
-        opacity: 1;
-        transform: translateY(0);
-    }
-}
-</style>
 
 <script setup lang="ts">
 definePageMeta({
